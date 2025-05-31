32a35433f4ce0e26aa6a967ae09b0a772fd3cacc
Soroush Hashemifar <soroush.hashemi76@gmail.com>
2025-05-31 04:22:39 +0330

    fixed the shape bug in invert_attention_mask
    There was a bug in the `invert_attention_mask` function that occurs when the developer provides an `encoder_attention_mask` split according to the number of attention heads. In this scenario, the function assumes that the encoder mask is not provided per head, leading to the failure of all 2D and 3D predicate checks. As a result, the variable `encoder_extended_attention_mask` is never initialized, causing an error during execution.

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -1670,8 +1670,10 @@ class ModuleUtilsMixin:
         """
         if encoder_attention_mask.dim() == 3:
             encoder_extended_attention_mask = encoder_attention_mask[:, None, :, :]
-        if encoder_attention_mask.dim() == 2:
+        elif encoder_attention_mask.dim() == 2:
             encoder_extended_attention_mask = encoder_attention_mask[:, None, None, :]
+        else:
+            encoder_extended_attention_mask = encoder_attention_mask
         # T5 has a mask that can compare sequence ids, we can simulate this here with this transposition
         # Cf. https://github.com/tensorflow/mesh/blob/8d2465e9bc93129b913b5ccc6a59aa97abd96ec6/mesh_tensorflow
         # /transformer/transformer_layers.py#L270
